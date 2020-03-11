fcd39e4c0f75894d1919219b84d50440009d01eb
张超 <zhang3611@qq.com>
2020-03-11 13:50:54 +0800

    sync

--- test/test.js
+++ test/test.js
@@ -1,19 +1,69 @@
 import value from "../packages/value";
 
-describe("value.js", () => {
-  it("测试 a.b.c.e.f", () => {
-    for (let i = 0; i < 10000; i++) {
-      value.GetValue("a.b.c.e.f", { a: { b: { c: { d: { e: { f: 1 } } } } } });
+describe("value.js 值测试", () => {
+  it("测试 a.b.c.d.e.f", () => {
+    for (let i = 0; i < 1; i++) {
+      expect(value.GetValue("a.b.c.d.e.f", { a: { b: { c: { d: { e: { f: 1 } } } } } })).toBe(1);
     }
   });
-  it("测试 a.b.c.[0].e.f", () => {
-    for (let i = 0; i < 10000; i++) {
-      value.GetValue("a.b.c.[0].e.f", { a: { b: { c: [{ d: { e: { f: 1 } } }, { d: { e: { f: 1 } } }, { d: { e: { f: 1 } } }] } } });
+  it("测试 a.b.c.[0].d.e.f", () => {
+    for (let i = 0; i < 1; i++) {
+      expect(value.GetValue("a.b.c.[0].d.e.f", { a: { b: { c: [{ d: { e: { f: 1 } } }, { d: { e: { f: 1 } } }, { d: { e: { f: 1 } } }] } } })).toBe(1);
     }
   });
-  it("测试 a.b.c.[d.e.f === 1].e.f", () => {
-    for (let i = 0; i < 10000; i++) {
-      value.GetValue("a.b.c.[d.e.f === 1].e.f", { a: { b: { c: [{ d: { e: { f: 1 } } }, { d: { e: { f: 2 } } }, { d: { e: { f: 3 } } }] } } });
+  it("测试 a.b.c.[d.e.f === 1].d.e.f", () => {
+    for (let i = 0; i < 1; i++) {
+      expect(value.GetValue("a.b.c.[d.e.f === 1].d.e.f", { a: { b: { c: [{ d: { e: { f: 1 } } }, { d: { e: { f: 2 } } }, { d: { e: { f: 3 } } }] } } })).toBe(1);
+    }
+  });
+  it("测试 a.b.c.[k === 2].d.e.f", () => {
+    for (let i = 0; i < 1; i++) {
+      expect(
+        value.GetValue("a.b.c.[k === 2].d.e.f", {
+          a: {
+            b: {
+              c: [
+                { d: { e: { f: 1 } }, k: 2 },
+                { d: { e: { f: 2 } }, k: 3 },
+                { d: { e: { f: 3 } }, k: 4 }
+              ]
+            }
+          }
+        })
+      ).toBe(1);
+    }
+  });
+});
+
+describe("value.js 性能测试", () => {
+  it("测试 a.b.c.d.e.f", () => {
+    for (let i = 0; i < 100000; i++) {
+      value.GetValue("a.b.c.d.e.f", { a: { b: { c: { d: { e: { f: 1 } } } } } });
+    }
+  });
+  it("测试 a.b.c.[0].d.e.f", () => {
+    for (let i = 0; i < 100000; i++) {
+      value.GetValue("a.b.c.[0].d.e.f", { a: { b: { c: [{ d: { e: { f: 1 } } }, { d: { e: { f: 1 } } }, { d: { e: { f: 1 } } }] } } });
+    }
+  });
+  it("测试 a.b.c.[d.e.f === 2].d.e.f", () => {
+    for (let i = 0; i < 100000; i++) {
+      value.GetValue("a.b.c.[d.e.f === 2].d.e.f", { a: { b: { c: [{ d: { e: { f: 1 }, k: 2 } }, { d: { e: { f: 2 }, k: 3 } }, { d: { e: { f: 3 } }, k: 4 }] } } });
+    }
+  });
+  it("测试 a.b.c.[k === 2].d.e.f", () => {
+    for (let i = 0; i < 100000; i++) {
+      value.GetValue("a.b.c.[k === 2].d.e.f", {
+        a: {
+          b: {
+            c: [
+              { d: { e: { f: 1 } }, k: 2 },
+              { d: { e: { f: 2 } }, k: 3 },
+              { d: { e: { f: 3 } }, k: 4 }
+            ]
+          }
+        }
+      });
     }
   });
 });
