9a2f21a0f04243d89c43bf2469c52309f0584125
zhanghup <zhang3611@qq.com>
2020-04-15 21:55:54 +0800

    sync

--- packages/zpx.js
+++ packages/zpx.js
@@ -3,12 +3,6 @@ import initVue from "./init/vue";
 import initRouter from "./init/router.js";
 import initStore from "./init/store.js";
 
-import components from "./components/index.js";
-
-for (let k in components) {
-  Vue.component(k, components[k]);
-}
-
 let zpx = Vue;
 zpx.settings = {};
 initVue(zpx);
