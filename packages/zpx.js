6277345cf5e04c3a95499dc2a45058e206e1341c
张超 <zhang3611@qq.com>
2020-05-18 14:46:40 +0800

    sync

--- packages/zpx.js
+++ packages/zpx.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import initVue from "./init/vue";
 import initRouter from "./init/router.js";
 import initStore from "./init/store.js";
+import { GetValue, GetFormat } from "./init/value";
 
 let zpx = Vue;
 zpx.settings = {};
@@ -9,3 +10,5 @@ initVue(zpx);
 initStore(zpx);
 initRouter(zpx);
 export default zpx;
+
+export { GetValue, GetFormat };
