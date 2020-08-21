8afb407551e5012e6e377f6f8eb783f549d3ff78
张超 <zhang3611@qq.com>
2020-08-21 15:48:14 +0800

    sync

--- packages/zpx.js
+++ packages/zpx.js
@@ -4,10 +4,9 @@ import Promisem from "./init/promise";
 import ajax from "./init/utils/ajax.js";
 import utils from "./init/utils/index";
 
-export { GetValue, GetFormat, ajax };
+export { GetValue, GetFormat, Promisem, ajax };
 
 export default function(Vue, param = {}) {
-  window.Promisem = Promisem;
   Vue.prototype.$utils = utils;
   Vue.prototype.$v = GetValue;
   Vue.prototype.$f = GetFormat;
