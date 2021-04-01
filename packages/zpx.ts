57c4a62c7ad37e0d16f8e6b6ec5b4fe81ca6a276
zhanghup <zhang3611@qq.com>
2021-04-01 22:15:01 +0800

    s

--- packages/zpx.ts
+++ packages/zpx.ts
@@ -3,12 +3,55 @@ import Vals from './lib/vals'
 import ajax, {Input} from "./lib/ajax";
 import md5 from "./lib/md5.ts";
 import Promisem from "./lib/promisem"
-
+import {Ref} from 'vue';
 import DictFormat, {Dict} from "./lib/dict"
+import {RouteLocationNormalizedLoaded, RouterOptions, RouteLocationRaw, NavigationFailure} from "vue-router"
+
+
+interface IRouter {
+    readonly currentRoute: Ref<RouteLocationNormalizedLoaded>;
+    readonly options: RouterOptions;
+
+    push(to: RouteLocationRaw): Promise<NavigationFailure | void | undefined>;
+
+    replace(to: RouteLocationRaw): Promise<NavigationFailure | void | undefined>;
+
+    go(delta: number): void;
+
+}
+
+interface IStore {
+    readonly state: any;
+    readonly getters: any;
+
+    dispatch(type: string, payload?: any, options?: any): Promise<any>;
+
+    commit(type: string, payload?: any, options?: any): void;
+
+    [index: string]: any
+}
+
+export interface IGraphql {
+    query?(methodfields: string, variables?: any, level?: number): Promisem
+
+    mutate?(methodfields: string, variables?: any, level?: number): Promisem
+}
+
 
 export class Zpx {
     private __vf = new Vals()
     private __dict = new DictFormat()
+    private __router: IRouter
+    private __store: IStore
+    private __gqlfn: (key: string) => IGraphql;
+    private __gqlautofn: (opt: string, param?: any) => Promisem
+
+    constructor() {
+        this.__gqlfn = (key: string) => ({})
+        this.__gqlautofn = (opt: string, param?: any) => (new Promisem((resolve => resolve())))
+        this.__router = null
+        this.__store = null
+    }
 
     public val(value: any, ...keyOrFormat: string[]) {
         return this.__vf.$v(value, ...keyOrFormat)
@@ -26,7 +69,32 @@ export class Zpx {
         return toobj(input)
     }
 
-    public SetDict(dicts: Dict[]) {
+    public Router(): IRouter {
+        return this.__router
+    }
+
+    public Store(): IStore {
+        return this.__store
+    }
+
+
+    set gqlautofn(fn: (opt: string, param?: any) => Promisem) {
+        this.__gqlautofn = fn
+    }
+
+    set gqlfn(fn: (key: string) => IGraphql) {
+        this.__gqlfn = fn
+    }
+
+    set store(store: IStore) {
+        this.__store = store
+    }
+
+    set router(router: IRouter) {
+        this.__router = router
+    }
+
+    set dict(dicts: Dict[]) {
         this.__dict.SetDict(dicts)
         this.__vf.Dict(this.__dict)
     }
