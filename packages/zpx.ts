59bbaa7d481a623a583d2a44e7d45e48aef60ab3
zhanghup <zhang3611@qq.com>
2021-05-06 21:19:23 +0800

    s

--- packages/zpx.ts
+++ packages/zpx.ts
@@ -5,7 +5,7 @@ import md5 from "./lib/md5.ts";
 import Promisem from "./lib/promisem"
 import DictFormat, {Dict} from "./lib/dict"
 import {createStore, StoreOptions, Store} from 'vuex'
-import {createRouter, Router, RouterOptions, createWebHashHistory,RouteLocationNormalizedLoaded} from "vue-router";
+import {createRouter, Router, RouterOptions, createWebHashHistory, RouteLocationNormalizedLoaded} from "vue-router";
 import {Ref} from "vue"
 import mitt from 'mitt';
 
@@ -16,19 +16,14 @@ export interface IGraphql {
 }
 
 export class Zpx {
-    private __vf = new Vals()
-    private __dict_init = false
-    private __dict = new DictFormat()
-    private __store_init = false
-    private __store: Store<any> = createStore({})
-    private __router_init = false
-    private __router: Router = createRouter({history: createWebHashHistory(), routes: []})
-    private __apollo_client = new Map<string, IGraphql>()
-    private __apollo_client_init = false
-    private emitter: mitt.Emitter = mitt();
 
     constructor() {
     }
+
+    /** utils */
+    private __vf = new Vals()
+    private __dict_init = false
+    private __dict = new DictFormat()
 
     public val(value: any, ...keyOrFormat: string[]) {
         return this.__vf.$v(value, ...keyOrFormat)
@@ -46,6 +41,17 @@ export class Zpx {
         return toobj(input)
     }
 
+    public SetDict(dicts: Dict[]) {
+        this.__dict.SetDict(dicts)
+        this.__vf.Dict(this.__dict)
+        this.__dict_init = true
+    }/** utils end */
+
+
+    /** vuex */
+    private __store_init = false
+    private __store: Store<any> = createStore({})
+
     public store(...key: string[]): Store<any> | any {
         if (!this.__store_init) {
             console.error("[zpx] vuex未初始化")
@@ -56,18 +62,45 @@ export class Zpx {
         return this.__store
     }
 
+    public storeGet(key: string): any {
+        return this.__store.getters[key]
+    }
+
+    public InitStore<S>(options: StoreOptions<S>): Store<S> {
+        this.__store = createStore(options)
+        this.__store_init = true
+        return this.__store
+    }/** vuex end */
+
+
+    /** vue-router */
+    private __router_init = false
+    private __router: Router = createRouter({history: createWebHashHistory(), routes: []})
+
     public router(): Router {
         if (!this.__router_init) {
             console.error("[zpx] vue-router未初始化")
         }
         return this.__router
     }
-    public route(): Ref<RouteLocationNormalizedLoaded>{
+
+    public route(): Ref<RouteLocationNormalizedLoaded> {
         if (!this.__router_init) {
             console.error("[zpx] vue-router未初始化")
         }
         return this.__router.currentRoute
     }
+
+    public InitRouter(options: RouterOptions): Router {
+        this.__router = createRouter(options)
+        this.__router_init = true
+        return this.__router
+    }/** vue-router end */
+
+
+    /** apollo */
+    private __apollo_client = new Map<string, IGraphql>()
+    private __apollo_client_init = false
 
     public apo(args: string, param?: any): Promisem {
         if (!this.__apollo_client_init) {
@@ -117,45 +150,32 @@ export class Zpx {
         return v
     }
 
-    /** mitt 事件总线 */
-    public emit(ty: string|symbol, data: any) {
-        this.emitter.emit(ty, data)
-    }
-    public on(ty: string|symbol,fn: any){
-        this.emitter.on(ty,fn)
-    }
-    public off(ty: string|symbol,fn: any){
-        this.emitter.off(ty,fn)
-    }
-    public mittAll(){
-        return this.emitter.all
-    }
-    /** end */
-
-
-    public InitStore<S>(options: StoreOptions<S>): Store<S> {
-        this.__store = createStore(options)
-        this.__store_init = true
-        return this.__store
-    }
-
-    public InitRouter(options: RouterOptions): Router {
-        this.__router = createRouter(options)
-        this.__router_init = true
-        return this.__router
-    }
-
     public InitApolloClient(cli: Map<string, IGraphql>) {
         this.__apollo_client = cli
         this.__apollo_client_init = true
         return this.__apollo_client
+    }/** apollo end */
+
+
+    /** mitt 事件总线 */
+    private emitter: mitt.Emitter = mitt();
+
+    public emit(ty: string | symbol, data: any) {
+        this.emitter.emit(ty, data)
     }
 
-    public SetDict(dicts: Dict[]) {
-        this.__dict.SetDict(dicts)
-        this.__vf.Dict(this.__dict)
-        this.__dict_init = true
+    public on(ty: string | symbol, fn: any) {
+        this.emitter.on(ty, fn)
     }
+
+    public off(ty: string | symbol, fn: any) {
+        this.emitter.off(ty, fn)
+    }
+
+    public mittAll() {
+        return this.emitter.all
+    }/** mitt end */
+
 }
 
 let zz = new Zpx()
